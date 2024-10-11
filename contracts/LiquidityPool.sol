99450ce3c2a2d32ca289fbee85bba5d4e9393a17
ATikadze <a.tikadze@gmail.com>
2024-10-11 21:11:35 +0400

    * Added Reentrancy Guard * Other minor improvements

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "./LPToken.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 // TODO: Maybe add Ownable?
-// TODO: Add nonReentrant
-contract LiquidityPool
+contract LiquidityPool is ReentrancyGuard
 {
     uint256 constant tokensPerShare = 10; // Tokens per 1% share
     uint256 constant swapFee = 3; // 0.3 (will later divide the result by 10)
@@ -63,7 +63,7 @@ contract LiquidityPool
         return ((_token1Address == token1Address || _token2Address == token2Address) || (_token1Address == token2Address || _token2Address == token1Address));
     }
 
-    function tokensRatioValid(uint256 _token1Amount, uint256 _token2Amount) public view returns (bool)
+    function _tokensRatioValid(uint256 _token1Amount, uint256 _token2Amount) internal view returns (bool)
     {
         return token1.balanceOf(thisAddress) * _token2Amount == token2.balanceOf(thisAddress) * _token1Amount;
     }
@@ -81,9 +81,9 @@ contract LiquidityPool
             revert TransferFailed(_tokenAddress);
     }
 
-    function deposit(address _liquidityProvider, address _token1Address, address _token2Address, uint256 _token1Amount, uint256 _token2Amount) external validTokens(_token1Address, _token2Address)
+    function deposit(address _liquidityProvider, address _token1Address, address _token2Address, uint256 _token1Amount, uint256 _token2Amount) external validTokens(_token1Address, _token2Address) nonReentrant
     {
-        require(tokensRatioValid(_token1Amount, _token2Amount), "Ratio of the deposited tokens must match.");
+        require(_tokensRatioValid(_token1Amount, _token2Amount), "Ratio of the deposited tokens must match.");
         
         _deposit(_liquidityProvider, _token1Address, _token1Amount);
         _deposit(_liquidityProvider, _token2Address, _token2Amount);
@@ -95,7 +95,7 @@ contract LiquidityPool
 
     // -- LP Withdrawal methods --
 
-    function withdraw(address _liquidityProvider, uint256 _percentage) external
+    function withdraw(address _liquidityProvider, uint256 _percentage) external nonReentrant
     {
         require(_percentage > 0 && _percentage <= 100);
         
@@ -144,8 +144,7 @@ contract LiquidityPool
         return _getInAmount(_tokenInBalance, _tokenOutBalance, _tokenOutAmount);
     }
     
-    // TODO: Add LP fee
-    function swap(address _account, address _tokenInAddress, address _tokenOutAddress, uint256 _tokenInAmount, uint256 _tokenOutMinAmount) external validTokens(_tokenInAddress, _tokenOutAddress)
+    function swap(address _account, address _tokenInAddress, address _tokenOutAddress, uint256 _tokenInAmount, uint256 _tokenOutMinAmount) external validTokens(_tokenInAddress, _tokenOutAddress) nonReentrant
     {
         IERC20 _inToken = _getToken(_tokenInAddress);
 
